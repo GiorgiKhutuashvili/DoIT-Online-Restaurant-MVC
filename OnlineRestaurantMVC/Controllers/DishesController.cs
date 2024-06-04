8a40fd08d6aba5c1014eca577e7ba2061bbd39ad
Giorgi Khutuashvili <giorgix09@gmail.com>
2024-06-05 00:34:08 +0400

    added controller and view

---
+++ OnlineRestaurantMVC/Controllers/DishesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineRestaurantMVC.Data;
+
+namespace OnlineRestaurantMVC.Controllers
+{
+    public class DishesController : Controller
+    {
+        private readonly AppDbContext _context;
+        public DishesController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allDishes = await _context.Dishes.ToListAsync();
+            return View(allDishes);
+        }
+    }
+}
