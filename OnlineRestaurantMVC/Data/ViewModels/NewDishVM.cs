018b366e4d0c232d7994c71e59d6fcd25ca2903f
Giorgi Khutuashvili <giorgix09@gmail.com>
2024-06-05 16:30:36 +0400

    added create view

---
+++ OnlineRestaurantMVC/Data/ViewModels/NewDishVM.cs
@@ -0,0 +1,23 @@
+﻿using OnlineRestaurantMVC.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineRestaurantMVC.Data.ViewModels
+{
+    public class NewDishVM
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Description is required")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Price is required")]
+        public double Price { get; set; }
+        [Required(ErrorMessage = "Select the category")]
+        public DishCategory DishCategory { get; set; }
+        public bool IsNutty { get; set; }
+        public bool IsVegetarian { get; set; }
+        [Required(ErrorMessage = "Select the Pepper Level")]
+        public PepperLevel PepperLevel { get; set; }
+        [Required(ErrorMessage = "Image URL is required")]
+        public string ImageURL { get; set; }
+    }
+}
